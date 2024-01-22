fb254638ba56d2f2e576887761f1b257216f3d70
yutr0108 <144692127+yuta0108@users.noreply.github.com>
2024-01-22 15:20:38 +0000

    [finish]this

--- practice-variable/app.js
+++ practice-variable/app.js
@@ -1,8 +1,17 @@
-$(function (){
-  $('.box1').on('click', function (){
-    $('.box1').addClass('box1-ext');
+$(function(){
+  $('.bg1').on('click', function(){
+    $('.bg1').slideUp();
   });
-  $('.box1').mouseout(function (){
-    $('.box1').removeClass('box1-ext');
+
+  $('.bg2').on('click', function (){
+    $('.bg2').slideUp();
+  });
+
+  $('.bg3').on('click', function (){
+    $('.bg3').slideUp();
+  });
+
+  $('.bg4').on('click', function (){
+    $('.bg4').slideUp();
   });
 });
