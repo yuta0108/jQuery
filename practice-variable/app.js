70ab77a8da8f7fb15781f2704f191bc33c72dd7e
yutr0108 <144692127+yuta0108@users.noreply.github.com>
2024-01-22 14:04:58 +0000

    [finish]マウスホバー

--- practice-variable/app.js
+++ practice-variable/app.js
@@ -1,9 +1,8 @@
 $(function (){
-  $('.box1').slideDown(function (){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'width': '200px',
-      'height': '100px'
-    }).slideUp();
+  $('.box1').mouseover(function (){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function (){
+    $('.box1').removeClass('box1-ext');
   });
 });
